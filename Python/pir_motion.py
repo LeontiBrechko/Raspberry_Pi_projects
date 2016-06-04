963a2de1b8129b3c0b6d69f2d7e902c60673de44
leontibrechko <leontibrechko@gmail.com>
2016-06-04 16:45:31 -0400

    - basic python projects

--- Python/pir_motion.py
+++ Python/pir_motion.py
@@ -6,6 +6,6 @@ import time
 pir = MotionSensor(4)
 
 while True:
-	if pir.motion_detected:
-		print(time.asctime(time.localtime(time.time())))
-		sleep(1)
+    if pir.motion_detected:
+        print(time.asctime(time.localtime(time.time())))
+        sleep(1)
--- Python/traffic_lights.py
+++ Python/traffic_lights.py
@@ -2,32 +2,36 @@ from gpiozero import Button, LED, TrafficLights
 from time import sleep
 from sys import exit
 
+
 def btn_clicked():
-        lights.off()
-        quit()
+    lights.off()
+    quit()
+
 
 button = Button(21)
 lights = TrafficLights(17, 27, 22)
 
-button.when_released = btn_clicked 
+button.when_released = btn_clicked
+
 
 def main():
-	print('Ready to use!')
-	while True:
-		#button.wait_for_press()
-		#print('Button pressed')
-		lights.green.on()
-		sleep(1)
-		lights.green.off()
-		lights.amber.on()
-		sleep(1)
-		lights.amber.off()
-		lights.red.on()
-		sleep(2)
-		lights.amber.on()
-		sleep(1)
-		lights.off()
+    print('Ready to use!')
+    while True:
+        # button.wait_for_press()
+        # print('Button pressed')
+        lights.green.on()
+        sleep(1)
+        lights.green.off()
+        lights.amber.on()
+        sleep(1)
+        lights.amber.off()
+        lights.red.on()
+        sleep(2)
+        lights.amber.on()
+        sleep(1)
+        lights.off()
+
 
 if __name__ == '__main__':
-	print('Starting...')
-	main()
+    print('Starting...')
+    main()
